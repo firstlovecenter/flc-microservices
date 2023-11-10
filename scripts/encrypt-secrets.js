0384ba70191f4d3e59e6c82af390468d87a517ad
John-Dag Addy <jaedagy@gmail.com>
2023-11-10 13:02:15 +0000

    Update build and functions configuration, add notify function and secrets encryption script

--- scripts/encrypt-secrets.js
+++ scripts/encrypt-secrets.js
@@ -4,8 +4,8 @@ const secrets = require('gitops-secrets')
 async function main() {
   const payload = await secrets.providers.doppler.fetch()
   secrets.build(payload, { path: 'lib/secrets.js' })
-  secrets.build(payload, { path: 'payment/secrets.js' })
-  secrets.build(payload, { path: 'notify/secrets.js' })
+  secrets.build(payload, { path: 'functions/payment/secrets.js' })
+  secrets.build(payload, { path: 'functions/notify/secrets.js' })
 }
 
 main()
