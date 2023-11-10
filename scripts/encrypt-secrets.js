f947429adc1a469c5d5ce7f590257f1c6714f67e
John-Dag Addy <jaedagy@gmail.com>
2023-11-10 13:15:00 +0000

    Comment out unused secrets.build call in encrypt-secrets.js

--- scripts/encrypt-secrets.js
+++ scripts/encrypt-secrets.js
@@ -4,7 +4,7 @@ const secrets = require('gitops-secrets')
 async function main() {
   const payload = await secrets.providers.doppler.fetch()
   secrets.build(payload, { path: 'lib/secrets.js' })
-  secrets.build(payload, { path: 'functions/payment/secrets.js' })
+  // secrets.build(payload, { path: 'functions/payment/secrets.js' })
   secrets.build(payload, { path: 'functions/notify/secrets.js' })
 }
 
